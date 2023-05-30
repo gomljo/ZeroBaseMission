eb8069b9efd574ca9ea339755dcf6c51f00c0773
gomljo <gomljo0307@naver.com>
2023-05-30 15:24:15 +0900

    test: 공공 데이터 REST API를 활용하여 불러오기 테스트
    REST API를 이용하여 공공 데이터를 불러올 때 발생할 수 있는 시나리오에 대한 테스트를 진행

--- PublicWifiSearch/src/test/java/com/publicwifisearch/domain/publicWifi/AddressTest.java
+++ PublicWifiSearch/src/test/java/com/publicwifisearch/domain/publicWifi/AddressTest.java
@@ -1,23 +1,44 @@
 package com.publicwifisearch.domain.publicWifi;
 
+import com.publicwifisearch.domain.publicWifi.information.address.Address;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AddressTest {
+    String district;
+    String roadAddress;
+    String detailAddress;
+    @BeforeEach
+    public void 주소_값_선언(){
+        district = "서대문구";
+        roadAddress = "서소문로 11";
+        detailAddress = "서소문로 11 5";
+    }
 
     @DisplayName("Address 객체 생성 테스트")
     @Test
-    public void 입력된_주소와_객체의_값이_다르면_false를_리턴(){
-        String district = "서대문구";
-        String roadAddress = "서소문로 11";
-        String detailAddress = "서소문로 11 5";
+    public void 입력된_주소들과_객체의_값이_다르면_false를_리턴(){
 
         Address address = new Address(district, roadAddress, detailAddress);
         assertEquals(address.getDistrict(), district );
         assertEquals(address.getRoadAddress(), roadAddress);
         assertEquals(address.getDetailAddress(), detailAddress);
+    }
+
+    @DisplayName("Address 객체 빌더 생성 테스트")
+    @Test
+    public void 빌더를_이용하여_생성한_객체의_정보가_입력정보와_다르면_false를_리턴(){
+        Address address1 = Address.builder()
+                .district(district)
+                .roadAddress(roadAddress)
+                .detailAddress(detailAddress)
+                .build();
+        assertEquals(address1.getDistrict(), district );
+        assertEquals(address1.getRoadAddress(), roadAddress);
+        assertEquals(address1.getDetailAddress(), detailAddress);
     }
 
 }
