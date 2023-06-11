3c38930f6178b688834783420e035d9e349fc770
gomljo <gomljo0307@naver.com>
2023-06-12 05:29:16 +0900

    feat: 검색 기록 도메인 정의

--- PublicWifiSearch/src/main/java/com/publicWifiSearch/domain/model/history/SearchHistory.java
+++ PublicWifiSearch/src/main/java/com/publicWifiSearch/domain/model/history/SearchHistory.java
@@ -4,11 +4,28 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @Getter
 @RequiredArgsConstructor
 @Builder
 public class SearchHistory {
+    private int id;
     private final Coordinate coordinate;
     private final String searchTime;
 
+    public SearchHistory(int id, Coordinate coordinate, String searchTime) {
+        this.id = id;
+        this.coordinate = coordinate;
+        this.searchTime = searchTime;
+    }
+
+    @Override
+    public String toString() {
+        return "SearchHistory{" +
+                "id=" + id +
+                ", coordinate=" + coordinate +
+                ", searchTime='" + searchTime + '\'' +
+                '}';
+    }
 }
